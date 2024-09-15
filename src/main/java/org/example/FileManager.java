da183046ae050b74a3aae26031b4e2cf26c4a9a3
Viktor-Ryabov <iprva@ya.ru>
2024-09-15 22:53:17 +0300

    added a part of FileManager class

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,16 +1,21 @@
 package org.example;
 
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class FileManager implements Runnable {
+    ArrayList<String> fileData = new ArrayList<>();
     public void run() {
         try(BufferedReader reader = new BufferedReader(new FileReader(InitialData.fileAdress))){
             String line;
             while((line = reader.readLine()) != null){
                 System.out.println(line + "\n");
+                fileData.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(fileData);
     }
 }
